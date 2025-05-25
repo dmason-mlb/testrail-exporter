455b860245b622ad52c0f1b36ea9929a1b7c8d30
Doug Mason <Douglas.Mason@mlb.com>
2025-05-25 03:03:45 -0600

    increment build number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="testrail-exporter",
-    version="1.1.55",
+    version="1.2.1",
     packages=find_packages(),
     package_data={
         "": ["*.png", "*.ico", "*.icns"],
